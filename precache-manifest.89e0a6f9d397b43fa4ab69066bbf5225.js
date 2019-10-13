bf625dd26471316316366eecc4d194a37a77fe12
NicholusMuwonge <nicholusmuwonge@gmail.com>
2019-10-13 21:55:54 +0300

    Updates

---
+++ precache-manifest.89e0a6f9d397b43fa4ab69066bbf5225.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6483a1f918eca565647ac89d998a0a07",
+    "url": "/NicholusMuwonge/Portifolio-Website/index.html"
+  },
+  {
+    "revision": "0f31351e452c48343147",
+    "url": "/NicholusMuwonge/Portifolio-Website/static/css/main.86266998.chunk.css"
+  },
+  {
+    "revision": "290d1fda287535a2578d",
+    "url": "/NicholusMuwonge/Portifolio-Website/static/js/2.9de77db1.chunk.js"
+  },
+  {
+    "revision": "0f31351e452c48343147",
+    "url": "/NicholusMuwonge/Portifolio-Website/static/js/main.5ffe0967.chunk.js"
+  },
+  {
+    "revision": "19563f218cde93255e19",
+    "url": "/NicholusMuwonge/Portifolio-Website/static/js/runtime-main.d9c00d81.js"
+  },
+  {
+    "revision": "e4478fb44288d97519aed619eb982f15",
+    "url": "/NicholusMuwonge/Portifolio-Website/static/media/black.e4478fb4.png"
+  },
+  {
+    "revision": "0be4f42bb6b5be2cfc5262da3da6e98b",
+    "url": "/NicholusMuwonge/Portifolio-Website/static/media/nicks.0be4f42b.png"
+  }
+]);
